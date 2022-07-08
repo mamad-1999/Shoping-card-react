3906e825602f08d46ca75890cf3a52f113e9e290
pc <www.mmhmdmm83@gmail.com>
2022-07-08 09:49:08 +0430

    modified Search products 'by changing the route reset the search State to Show all products'

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import "./SearchBar.css";
 import { FiSearch } from "react-icons/fi";
 import { FilterContext } from "../Context/ContextFilter";
+import { useLocation } from "react-router-dom";
 
 export default function SearchBar() {
   const { dispath } = useContext(FilterContext);
+
+  const location = useLocation();
+  const { pathname } = location;
+  // change The route search input is empty
+  useEffect(() => {
+    dispath({ type: "SEARCH_KEYWORD", payload: "" });
+  }, [pathname]);
 
   const searchKeywordHandler = (e) => {
     dispath({ type: "SEARCH_KEYWORD", payload: e.target.value });
